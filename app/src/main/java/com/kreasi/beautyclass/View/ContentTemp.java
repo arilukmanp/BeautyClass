c3c0ffef66b4e1f764f21743e877a568d8159a54
ozianf101 <ahnufasmada01@gmail.com>
2019-04-09 16:03:51 +0700

    add action bar

--- app/src/main/java/com/kreasi/beautyclass/View/ContentTemp.java
+++ app/src/main/java/com/kreasi/beautyclass/View/ContentTemp.java
@@ -2,6 +2,8 @@ package com.kreasi.beautyclass.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.kreasi.beautyclass.R;
 
@@ -11,5 +13,26 @@ public class ContentTemp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_temp);
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.about){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
