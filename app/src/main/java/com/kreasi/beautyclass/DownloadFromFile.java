4b4c57d130b6736fbc39a3d4a6125376ad6ab685
ozianf101 <ahnufasmada01@gmail.com>
2019-05-18 15:48:27 +0700

    download Image

---
+++ app/src/main/java/com/kreasi/beautyclass/DownloadFromFile.java
@@ -0,0 +1,83 @@
+package com.kreasi.beautyclass;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class DownloadFromFile extends AsyncTask<String, String, String> {
+    private ProgressDialog pDialog;
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        System.out.println("Starting download");
+    }
+
+    @Override
+    protected String doInBackground(String... f_url) {
+        int count;
+        try {
+            String root = Environment.getExternalStorageDirectory().toString();
+
+            System.out.println("Downloading");
+            URL url = new URL(f_url[0]);
+
+            URLConnection conection = url.openConnection();
+            conection.connect();
+            // getting file length
+            int lenghtOfFile = conection.getContentLength();
+
+            // input stream to read file - with 8k buffer
+            InputStream input = new BufferedInputStream(url.openStream(), 8192);
+
+            // Output stream to write file
+
+            OutputStream output = new         FileOutputStream(root+"/downloadedfile.jpg");
+            byte data[] = new byte[1024];
+
+            long total = 0;
+            while ((count = input.read(data)) != -1) {
+                total += count;
+
+                // writing data to file
+                output.write(data, 0, count);
+
+            }
+
+            // flushing output
+            output.flush();
+
+            // closing streams
+            output.close();
+            input.close();
+
+        } catch (Exception e) {
+            Log.e("Error: ", e.getMessage());
+        }
+
+        return null;
+    }
+    protected void onProgressUpdate(String... progress) {
+        // setting progress percentage
+        pDialog.setProgress(Integer.parseInt(progress[0]));
+    }
+
+    /**
+     * After completing background task
+     * Dismiss the progress dialog
+     * **/
+    @Override
+    protected void onPostExecute(String file_url) {
+        // dismiss the dialog after the file was downloaded
+       // dismissDialog(progress_bar_type);
+        System.out.println("Downloaded");
+    }
+}
--- app/src/main/java/com/kreasi/beautyclass/View/NewGallery.java
+++ app/src/main/java/com/kreasi/beautyclass/View/NewGallery.java
@@ -1,12 +1,18 @@
 package com.kreasi.beautyclass.View;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,19 +21,30 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.github.chrisbanes.photoview.PhotoView;
 import com.kreasi.beautyclass.Data.GalleryData;
+import com.kreasi.beautyclass.DownloadFromFile;
 import com.kreasi.beautyclass.ItemClickSupport;
 import com.kreasi.beautyclass.Model.GalleryModel;
 import com.kreasi.beautyclass.Presenter.GalleryAdapter;
 import com.kreasi.beautyclass.R;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.List;
 
-public class NewGallery extends AppCompatActivity {
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class NewGallery extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
     private RecyclerView rvGallery;
     private ArrayList<GalleryModel> listGal = new ArrayList<>();
     GalleryAdapter galleryAdapter;
@@ -35,6 +52,10 @@ public class NewGallery extends AppCompatActivity {
     private Boolean chooseMode=false;
     ImageButton closeButton;
     ImageView gmb;
+    private ProgressDialog pDialog;
+    private static final int WRITE_REQUEST_CODE = 300;
+    private static final String TAG = NewGallery.class.getSimpleName();
+    int posGmb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +92,7 @@ public class NewGallery extends AppCompatActivity {
                     layer.setMaxHeight(gmb.getMaxHeight());
                     layer.setMaxWidth(gmb.getMaxWidth());
                 }else if (chooseMode == false){
+                    posGmb = position;
                     showSelectedItem(listGal.get(position));
                 }
             }
@@ -109,7 +131,7 @@ public class NewGallery extends AppCompatActivity {
 
     }
 
-    private void showSelectedItem(GalleryModel galleryModel) {
+    private void showSelectedItem(final GalleryModel galleryModel) {
 
         final Dialog dialog = new Dialog(NewGallery.this);
         dialog.setContentView(R.layout.pick_foto);
@@ -121,6 +143,20 @@ public class NewGallery extends AppCompatActivity {
                 .apply(new RequestOptions().override(500,500))
                 .into(photoView);
         TextView downButton = dialog.findViewById(R.id.downloadBtn);
+        downButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (EasyPermissions.hasPermissions(NewGallery.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    //Get the URL entered
+                    new DownloadFileFromURL().execute(galleryModel.getImgUrl());
+
+                } else {
+                    //If permission is not present request for the same.
+                    EasyPermissions.requestPermissions(NewGallery.this, getString(R.string.write_file),WRITE_REQUEST_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
+                }
+                //new DownloadFileFromURL().execute(galleryModel.getImgUrl());
+            }
+        });
         ImageView closePrev = dialog.findViewById(R.id.prevImgCLS);
         closePrev.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -159,5 +195,106 @@ public class NewGallery extends AppCompatActivity {
             menu.findItem(R.id.rate).setVisible(false);
         }
         return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        return super.onCreateDialog(id);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, NewGallery.this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+
+    }
+
+    class DownloadFileFromURL extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            System.out.println("Starting download");
+
+            pDialog = new ProgressDialog(NewGallery.this);
+            pDialog.setMessage("Loading... Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * Downloading file in background thread
+         * */
+        @Override
+        protected String doInBackground(String... f_url) {
+            int count;
+            try {
+                String root = Environment.getExternalStorageDirectory().toString();
+
+                System.out.println("Downloading");
+                URL url = new URL(f_url[0]);
+
+                URLConnection conection = url.openConnection();
+                conection.connect();
+                // getting file length
+                int lenghtOfFile = conection.getContentLength();
+
+                // input stream to read file - with 8k buffer
+                InputStream input = new BufferedInputStream(url.openStream(), 8192);
+
+                // Output stream to write file
+
+                OutputStream output = new FileOutputStream(root+"/downloaded" + lenghtOfFile + ".jpg");
+                byte data[] = new byte[1024];
+
+                long total = 0;
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+
+                    // writing data to file
+                    output.write(data, 0, count);
+
+                }
+
+                // flushing output
+                output.flush();
+
+                // closing streams
+                output.close();
+                input.close();
+
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
+            }
+
+            return null;
+        }
+
+
+
+        /**
+         * After completing background task
+         * **/
+        @Override
+        protected void onPostExecute(String file_url) {
+            System.out.println("Downloaded");
+            Toast.makeText(NewGallery.this,"Download Success",Toast.LENGTH_SHORT).show();
+            pDialog.dismiss();
+        }
+
     }
 }
